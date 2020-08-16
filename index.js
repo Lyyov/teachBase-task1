9d7937d793aae7afe56676edfd09ce8a09cda87d
Lyyov <lyyovghidora@gmail.com>
2020-08-16 21:31:49 +0300

    first module created with class

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ const node = document.createElement("H1");
 const textnode = document.createTextNode("First task from Teachbase");
 
 node.appendChild(textnode);
-document.getElementById("root").appendChild(node)
+document.getElementById("root").appendChild(node);
+
+import Rectangle from "./src/newModule";
+
+const square = new Rectangle(20, 20);
+
+console.log(square.calcArea())
